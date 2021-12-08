de6ec45787535e33511a537d5d486d78f8e0fcac
Manas Das <manas.das@cyberark.com>
2021-12-08 22:07:42 +0530

    updated LoginOptions UI

--- app/src/main/java/com/cyberark/mfa/LoginOptionsActivity.kt
+++ app/src/main/java/com/cyberark/mfa/LoginOptionsActivity.kt
@@ -1,6 +1,9 @@
 package com.cyberark.mfa
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.cardview.widget.CardView
 import com.cyberark.mfa.utils.AppConfig
 
@@ -9,8 +12,35 @@ class LoginOptionsActivity : HomeActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_options)
         val account =  AppConfig.setupAccountFromSharedPreference(this)
-        findViewById<CardView>(R.id.loginexternal).setOnClickListener {
+        findViewById<CardView>(R.id.cv_redirect_login).setOnClickListener {
             login(account)
         }
+        inflateMenuFromToolbar()
     }
+
+    // **************** Handle menu settings click action Start *********************** //
+
+    private fun inflateMenuFromToolbar() {
+        val toolbar:Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        if (supportActionBar != null) {
+            supportActionBar?.title = "Acme"
+        }
+        toolbar.inflateMenu(R.menu.settings_menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            //Start Settings activity
+            val intent = Intent(this, SettingsActivity::class.java)
+            intent.putExtra("from_activity", "HomeActivity")
+            startActivity(intent)
+            finish()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+    // **************** Handle menu settings click action End *********************** //
 }
